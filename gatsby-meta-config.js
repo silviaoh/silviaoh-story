0215f7ff52033235fe8a2b64802d9a37688e41b1
silviaoh <silvia.jisu.oh@gmail.com>
2021-09-07 15:26:56 +0900

    Modify: initial File blog info settings

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,22 +1,31 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `Silvia Dev. Story`,
+  description: `편안함과 신뢰감을 전하고 싶은 FronEnd-Developer.`,
+  author: `Silvia.oh`,
+  introduction: `편안함과 신뢰감을 전하고 싶은 FronEnd-Developer.`,
+  siteUrl: `https://silviaoh.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `https://github.com/silviaoh`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
     instagram: ``, // Your Instagram account
   },
   icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
+  keywords: [
+    `blog`,
+    `silvia`,
+    `developer`,
+    `개발`,
+    `지수`,
+    `Jisu`,
+    `프론트엔드`,
+    `Frontend`,
+  ],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'https://github.com/silviaoh', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="https://github.com/silviaoh"
       className="github"
       aria-label="GitHub"
       target="_blank"
